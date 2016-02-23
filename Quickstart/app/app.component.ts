220f8745e15c714bd62f5133e1bb2b26b6aa550e
Luke Rielley <lrielley@agl.com.au>
2016-02-24 08:50:47 +1100

    Created an Interface

--- Quickstart/app/app.component.ts
+++ Quickstart/app/app.component.ts
@@ -1,5 +1,10 @@
 import {Component} from 'angular2/core';
 
+interface Hero {
+    id: number;
+    name: string;
+}
+
 @Component({
     selector: 'my-app',
     template: '<h1>{{title}}</h1><h2>{{hero}} details!</h2>'
