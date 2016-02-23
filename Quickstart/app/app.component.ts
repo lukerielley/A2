a18134df0c3088825d266468d0be3e366530398a
Luke Rielley <lrielley@agl.com.au>
2016-02-24 08:55:02 +1100

    Added an input control to our component template

--- Quickstart/app/app.component.ts
+++ Quickstart/app/app.component.ts
@@ -11,7 +11,10 @@ interface Hero {
         <h1>{{title}}</h1>
         <h2>{{hero.name}} details!</h2>
         <div><label>id: </label>{{hero.id}}</div>
-        <div><label>name: </label>{{hero.name}}</div>
+        <div>
+        <label>name: </label>
+        <div><input value="{{hero.name}}" placeholder="name"></div>
+        </div>
         `
 })
 
