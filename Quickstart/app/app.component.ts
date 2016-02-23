08389380634070d65cb9bdbbd141013485902d0f
Luke Rielley <lrielley@agl.com.au>
2016-02-24 08:55:41 +1100

    Added 2-way binding to our input box

--- Quickstart/app/app.component.ts
+++ Quickstart/app/app.component.ts
@@ -13,7 +13,9 @@ interface Hero {
         <div><label>id: </label>{{hero.id}}</div>
         <div>
         <label>name: </label>
-        <div><input value="{{hero.name}}" placeholder="name"></div>
+        <div>
+        <input [(ngModel)]="hero.name" placeholder="name">
+        </div>
         </div>
         `
 })
