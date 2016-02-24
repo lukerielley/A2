5fdf7f699164aa2f784a078bc84c67fffff73e88
Luke Rielley <lrielley@agl.com.au>
2016-02-24 13:12:37 +1100

    Moved styles to seperate CSS file

--- Quickstart/app/Components/heroes.component.js
+++ Quickstart/app/Components/heroes.component.js
@@ -52,7 +52,9 @@ System.register(['angular2/core', './hero-detail.component', '../services/hero.s
                         // our DI declarations
                         // providers: [HeroService], THIS IS REMOVED FROM HERE, AS IT IS NOW DECLARED HIGHER IN APP.COMPONENT
                         templateUrl: './app/templates/heroes.component.html',
-                        styles: ["\n  .selected {\n    background-color: #CFD8DC !important;\n    color: white;\n  }\n  .heroes {\n    margin: 0 0 2em 0;\n    list-style-type: none;\n    padding: 0;\n    width: 10em;\n  }\n  .heroes li {\n    cursor: pointer;\n    position: relative;\n    left: 0;\n    background-color: #EEE;\n    margin: .5em;\n    padding: .3em 0em;\n    height: 1.6em;\n    border-radius: 4px;\n  }\n  .heroes li.selected:hover {\n    color: white;\n  }\n  .heroes li:hover {\n    color: #607D8B;\n    background-color: #EEE;\n    left: .1em;\n  }\n  .heroes .text {\n    position: relative;\n    top: -3px;\n  }\n  .heroes .badge {\n    display: inline-block;\n    font-size: small;\n    color: white;\n    padding: 0.8em 0.7em 0em 0.7em;\n    background-color: #607D8B;\n    line-height: 1em;\n    position: relative;\n    left: -1px;\n    top: -4px;\n    height: 1.8em;\n    margin-right: .8em;\n    border-radius: 4px 0px 0px 4px;\n  }\n"]
+                        styleUrls: [
+                            'app/styles/heroes.component.css'
+                        ],
                     }), 
                     __metadata('design:paramtypes', [hero_service_1.HeroService])
                 ], HeroesComponent);
