fa5dcd93b24606cb9806dd0331282de165dedb56
Luke Rielley <luke.rielley@gmail.com>
2016-06-15 20:19:08 +1000

    Removed in memory service

--- RC1-Quickstart/app/main.ts
+++ RC1-Quickstart/app/main.ts
@@ -2,9 +2,6 @@
 import { provide }    from '@angular/core';
 import { XHRBackend } from '@angular/http';
 
-import { InMemoryBackendService, SEED_DATA } from 'angular2-in-memory-web-api';
-import { InMemoryDataService }               from './in-memory-data.service';
-
 // The usual bootstrapping imports
 import { bootstrap }      from '@angular/platform-browser-dynamic';
 import { HTTP_PROVIDERS } from '@angular/http';
@@ -12,7 +9,5 @@ import { HTTP_PROVIDERS } from '@angular/http';
 import { AppComponent }   from './app.component';
 
 bootstrap(AppComponent, [
-    HTTP_PROVIDERS,
-    //provide(XHRBackend, { useClass: InMemoryBackendService }), // in-mem server
-    //provide(SEED_DATA,  { useClass: InMemoryDataService })     // in-mem server data
+    HTTP_PROVIDERS
 ]);
